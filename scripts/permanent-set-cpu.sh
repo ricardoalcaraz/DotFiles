322fd689b008a9de4b9347013738a158522a4662
ralcaraz <ralcarazerhs@gmail.com>
2022-08-06 17:35:40 -0700

    fixed set cpu script

--- scripts/permanent-set-cpu.sh
+++ scripts/permanent-set-cpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-systemctl set-property -- user.slice AllowedCPUs=16-19
-systemctl set-property -- system.slice AllowedCPUs=16-19
-systemctl set-property -- init.scope AllowedCPUs=16-19
-systemctl set-property -- high-performance.slice AllowedCPUs=0-15
-systemctl set-property -- full-cpu.slice AllowedCPUs=0-19
+systemctl set-property user.slice AllowedCPUs=16-19
+systemctl set-property system.slice AllowedCPUs=16-19
+systemctl set-property init.scope AllowedCPUs=16-19
+systemctl set-property high-performance.slice AllowedCPUs=0-15
+systemctl set-property full-cpu.slice AllowedCPUs=0-19
