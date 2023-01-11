f19c5aab22de9c3ff7a524949610f753e79a83c4
ralcaraz <ralcarazerhs@gmail.com>
2023-01-10 23:08:48 -0800

    updated files

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-rsync -a --delete --quiet -e ssh /path/to/backup remoteuser@remotehost:/location/of/backup
+rsync --quiet -a --numeric-ids --delete -e ssh /var/lib/grafana/* server@camus:/volume2/linux/grafana/
+rsync --quiet -a --numeric-ids --delete -e ssh /var/lib/prometheus/* server@camus:/volume2/linux/prometheus/
+rsync --quiet -a --numeric-ids --delete -e ssh /var/lib/postgres/* server@camus:/volume2/linux/postgres/
