aae908f7c193f8482c092ccabfe7fdc90cb9fbd7
ralcaraz <ralcarazerhs@gmail.com>
2023-01-20 18:24:48 -0800

    added config files

--- scripts/set-allowed-cpu.sh
+++ scripts/set-allowed-cpu.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-systemctl set-property --runtime -- user.slice AllowedCPUs=16-19
+systemctl set-property --runtime -- user.slice AllowedCPUs=0-7,16-19
 systemctl set-property --runtime -- system.slice AllowedCPUs=16-19
 systemctl set-property --runtime -- init.scope AllowedCPUs=16-19
-systemctl set-property --runtime -- high-performance.slice AllowedCPUs=0-15
-systemctl set-property --runtime -- full-cpu.slice AllowedCPUs=0-19
+systemctl set-property --runtime -- cloud.slice AllowedCPUs=0-15
