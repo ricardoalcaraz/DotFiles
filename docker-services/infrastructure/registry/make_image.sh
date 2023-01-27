6410733a3277644a0ec431e336e5b42993a5b308
ralcaraz <ralcarazerhs@gmail.com>
2023-01-26 21:32:49 -0800

    temp

--- docker-services/infrastructure/registry/make_image.sh
+++ docker-services/infrastructure/registry/make_image.sh
@@ -1,2 +1,4 @@
-docker build -t registry.ricardoalcaraz.dev/registry:latest ./
-docker run -p 80:80 -p 443:443 -it -v ~/.local/registry:/var/lib/registry registry.ricardoalcaraz.dev/registry:latest 
+#docker build -t registry.ricardoalcaraz.dev/registry:latest ./
+docker build -t registry.digitalocean.com/rar-tech/registry:s3 ./
+docker image push registry.digitalocean.com/rar-tech/registry:s3
+#docker run -p 80:80 -p 443:443 -it -v ~/.local/registry:/var/lib/registry registry.ricardoalcaraz.dev/registry:latest 
