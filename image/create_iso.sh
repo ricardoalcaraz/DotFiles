0b5e9b65f8455814e33ad55d2a278e9d43e0c9fb
ralcaraz <ralcarazerhs@gmail.com>
2024-02-25 20:01:17 -0800

    removed existing iso image
    added archiso creation script
    
    added arch install scripts
    
    fixed .gitignore

---
+++ image/create_iso.sh
@@ -0,0 +1,3 @@
+# create iso command
+#mkarchiso -v -w /tmp/archiso-tmp /path/to/profile/
+sudo mkarchiso -v -w /tmp/archiso-tmp ./archiso/
--- scripts/configuration/configure-ufw.sh
+++ scripts/configuration/configure-ufw.sh
@@ -1,6 +1,5 @@
 ufw default deny
 ufw allow from 192.168.1.0/24
 ufw allow from 63.205.195.240/28
-ufw allow Deluge
 ufw limit ssh
 ufw logging off
