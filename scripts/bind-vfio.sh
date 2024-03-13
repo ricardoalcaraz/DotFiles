411e86fb2e1f41f4232656815497214196f6f25a
ralcaraz <ralcarazerhs@gmail.com>
2024-03-13 16:02:22 -0700

    updated pkgbuild

--- scripts/bind-vfio.sh
+++ scripts/bind-vfio.sh
@@ -6,18 +6,19 @@ modprobe vfio_iommu_type1
 modprobe vfio_pci
 
 ## Unbind VGA and audio from "standard" drivers
-#echo 0000:01:00.0 > /sys/bus/pci/devices/0000:01:00.0/driver/unbind
-#echo 0000:01:00.1 > /sys/bus/pci/devices/0000:01:00.1/driver/unbind
+echo 0000:01:00.0 > /sys/bus/pci/devices/0000:01:00.0/driver/unbind
+echo 0000:01:00.1 > /sys/bus/pci/devices/0000:01:00.1/driver/unbind
 
 ##Bind VGA and audio to vfio-pci
-#echo 0000:01:00.0 > /sys/bus/pci/drivers/vfio-pci/bind
-#echo 0000:01:00.1 > /sys/bus/pci/drivers/vfio-pci/bind
+echo 0000:01:00.0 > /sys/bus/pci/drivers/vfio-pci/bind
+echo 0000:01:00.1 > /sys/bus/pci/drivers/vfio-pci/bind
 
+modprobe -r nvidia
 
 ##Intel Arc
 ## Unbind VGA and audio from "standard" drivers
-echo 0000:00:02.0 > /sys/bus/pci/devices/0000:00.02.0/driver/unbind
+#echo 0000:01:00.0 > /sys/bus/pci/devices/0000:01:00.0/driver/unbind
 
 ##Bind VGA and audio to vfio-pci
 #echo 0000:02.00.0 > /sys/bus/pci/drivers/vfio-pci/bind
-echo 0000:00:02.0 > /sys/bus/pci/drivers/vfio-pci/bind
+#echo 0000:01:00.0 > /sys/bus/pci/drivers/vfio-pci/bind
